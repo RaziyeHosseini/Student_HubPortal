e5271e403268e82197cd6216ef35e7241b8fd809
hosseiniraziye8@gmail.com <raziyehosseini@Raziyes-MacBook-Pro.local>
2025-11-03 10:55:21 -0800

    first commit

---
+++ src/Student_package/main.java
@@ -0,0 +1,41 @@
+/**
+* Lead Author(s):
+* @author raziyehosseini; student ID
+* @author Full name; student ID
+* <<Add additional lead authors here>>
+*
+* Other Contributors:
+* Full name; student ID or contact information if not in class
+* <<Add additional contributors (mentors, tutors, friends) here, with contact information>>
+*
+* References:
+* Morelli, R., & Walde, R. (2016).
+* Java, Java, Java: Object-Oriented Problem Solving
+* https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+*
+* <<Add more references here>>
+*
+* Version: 2025-11-03
+*/
+package Student_package;
+
+/**
+ * Purpose: The reponsibility of main is ...
+ *
+ * main is-a ...
+ * main is ...
+ */
+public class main
+{
+
+	/**
+	 * Purpose: 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
